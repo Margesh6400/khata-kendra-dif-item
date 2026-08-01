89f03031b507531824346906680730003c0e8450
Margesh6400 <margeshpolara@gmail.com>
2026-08-01 15:27:21 +0530

    chore: update Supabase credentials in .env and refresh service worker revision hash

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ppu4k293h0g"
+    "revision": "0.u8167fjg098"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
